4f8075207f5bd9806ad346107a7c62d5629a658b
femce4l20 <degendegeb@gmail.com>
2026-05-16 11:19:24 -0400

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,12 @@
+document.getElementById("apply").addEventListener("click", async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  if (!tab.url.includes("roblox.com")) {
+    alert("Open Roblox first!");
+    return;
+  }
+
+  chrome.tabs.sendMessage(tab.id, {
+    type: "APPLY_STYLES"
+  });
+});
