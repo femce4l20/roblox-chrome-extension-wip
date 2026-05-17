3b468a43bfeca804c5482e0122c94c140640b8b8
femce4l20 <degendegeb@gmail.com>
2026-05-16 20:31:01 -0400

    Update alert message for Roblox tab check

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ document.getElementById("apply").addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   if (!tab.url.includes("roblox.com")) {
-    alert("Open Roblox first!");
+    alert("Open Roblox first stupid!");
     return;
   }
 
